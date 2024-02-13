2730778c8c347c9c390c5b13ee29a6cd04c5a8db
Asir Ahbab Raiyan <ahbab.raiyan@gmail.com>
2024-02-13 21:42:53 +0600

    1st blood
    Completion of Caesar Cipher implementation

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,24 +1,48 @@
 alphabet=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q','r', 's', 't', 'u', 'v','w', 'x', 'y', 'z']
 
-def encrypt(plain_text,shift): #hello h=7
+def encrypt(plain_text,shift): 
     cipher_text=""
     for char in plain_text:
-        pos=alphabet.index(char)
-        new=(pos+shift)%26
-        cipher_text+=alphabet[new]
-    print("Here's the text after encryption:",cipher_text)
+        if char in alphabet:
+          pos=alphabet.index(char)
+          new=(pos+shift)%26
+          cipher_text+=alphabet[new]
+        elif(char.isupper()):
+           cipher_text += chr((ord(char) + shift-65) % 26 + 65)
+        else:
+           cipher_text+=char
+    print("Here's the text after encryption:",cipher_text)   ##Encryption function
 
 def decrypt(cipher_text,shift):
-    print('NICE')
+    plain_text=""
+    for c in cipher_text:
+        if c in alphabet:
+           p=alphabet.index(c)
+           n=(p-shift)%26
+           plain_text+=alphabet[n]
+        elif c.isupper():
+           plain_text+= chr((ord(c) - shift-65) % 26 + 65)
+        else:
+           plain_text+=c
+    print("The message after decryption is: ",plain_text)    ##Decryption function
 
-opt=input("Type 'e' for encryption & 'd' for decryption:\n")
-msg=input("Type your message:\n")
-key=int(input("Enter the value of shift key:\n"))
 
-if opt=='e':
-    encrypt(plain_text=msg,shift=key)
+flag=True
+while flag:
+    opt=input("Type 'enc' for encryption & 'dec' for decryption:\n")
+    msg=input("Type your message:\n")
+    key=int(input("Enter the value of shift key:\n"))        
 
-elif opt=='d':
-    decrypt(cipher_text=msg,shift=key)
+    if opt=='enc':
+      encrypt(plain_text=msg,shift=key)
+
+    elif opt=='dec':
+      decrypt(cipher_text=msg,shift=key)
+    
+    con=input("Type 'yes' to continue; type 'no' to exit\n")
+    if con=='no':
+      flag=False
+print("Good bye. Have a nice day!!")
+
 
 
